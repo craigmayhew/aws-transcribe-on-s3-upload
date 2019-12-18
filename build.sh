40d9700082ca57f4dcfced1a085c5829618a41cd
Craig Mayhew <craig@mayhew.io>
2019-12-18 23:37:26 +0000

    Created setup.sh to install python package and generate function.zip

---
+++ build.sh
@@ -0,0 +1,13 @@
+mkdir -p package
+
+# install 3rd party python package
+pip3 install --target ./package --user --install-option="--prefix=" tscribe
+
+# check syntax of script
+python3 -m py_compile lambda.py
+
+# copy lambda.py into package folder
+cp lambda.py package/
+
+# compress python lambda into zip
+cd package && zip -r9 ../function.zip .
