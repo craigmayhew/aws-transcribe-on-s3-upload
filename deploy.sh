ba7f1bd189026ebb0f6b9fe5694f3b3d88bf5bd8
Craig Mayhew <craig@mayhew.io>
2019-12-24 13:35:04 +0000

    Attempting to automatically update/deploy the cloudformation stack with travis

---
+++ deploy.sh
@@ -0,0 +1,55 @@
+# Thanks to https://gist.github.com/mdjnewman/b9d722188f4f9c6bb277a37619665e77 
+
+REGION=eu-west-2
+STACK_NAME=transcribe-on-s3-upload
+
+# check if cloudformation stack exists
+if ! aws cloudformation describe-stacks --region $REGION --stack-name $STACK_NAME ; then
+  # install stack if does not exist
+  echo -e "\nStack does not exist, creating ..."
+  aws cloudformation create-stack \
+    --region $REGION \
+    --stack-name $STACK_NAME \
+    template.yaml
+
+  echo "Waiting for stack to be created ..."
+  aws cloudformation wait stack-create-complete \
+    --region $REGION \
+    --stack-name $STACK_NAME \
+
+else
+  # update stack if it already exists
+  echo -e "\nStack exists, attempting update ..."
+
+  set +e
+  update_output=$( aws cloudformation update-stack \
+    --region $REGION \
+    --stack-name $STACK_NAME \
+    template.yaml  2>&1)
+  status=$?
+  set -e
+
+  echo "$update_output"
+
+  if [ $status -ne 0 ] ; then
+
+    # Don't fail for no-op update
+    if [[ $update_output == *"ValidationError"* && $update_output == *"No updates"* ]] ; then
+      echo -e "\nFinished create/update - no updates to be performed"
+      exit 0
+    else
+      exit $status
+    fi
+
+  fi
+
+
+  echo "Waiting for stack update to complete ..."
+  aws cloudformation wait stack-update-complete \
+    --region $REGION \
+    --stack-name $STACK_NAME \
+
+fi
+
+# once stack is ready, update lambda function with one we built in CI
+echo -e "\nStack ready ... Deploying freshly built lambda"
